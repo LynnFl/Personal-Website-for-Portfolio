6059defd1eaa83ef98ad0393c3f8d93dfed13ec5
LynnFl <lynn.fangling@gmail.com>
2023-12-02 17:00:25 +0800

    update

--- docs/script.js
+++ docs/script.js
@@ -2,3 +2,25 @@ script.js
 document.getElementById("changeTextButton").addEventListener("click", function() {
     document.getElementById("headerTitle").innerText = "Text Changed!";
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const gallery = document.querySelector('.image-gallery');
+
+    gallery.addEventListener('wheel', function(event) {
+        event.preventDefault();
+        gallery.scrollLeft += event.deltaY;
+    });
+
+});
+
+function adjustTitleSize() {
+    var screenWidth = window.innerWidth;
+    var fontSize = screenWidth; // 示例：将屏幕宽度除以10以确定字体大小
+    document.querySelector('.gallery-title').style.fontSize = fontSize + 'px';
+}
+
+// 当窗口大小改变时调整字体大小
+window.addEventListener('resize', adjustTitleSize);
+
+// 页面加载时调整字体大小
+adjustTitleSize();
