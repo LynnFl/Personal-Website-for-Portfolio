847c2f45af862f99ad1ad621763890fe944b45ea
LynnFl <lynn.fangling@gmail.com>
2024-01-04 13:05:25 +0800

    update

--- docs/script.js
+++ docs/script.js
@@ -24,3 +24,31 @@ window.addEventListener('resize', adjustTitleSize);
 
 // 页面加载时调整字体大小
 adjustTitleSize();
+
+
+function toggleImages() {
+    var container = document.getElementById("imageContainer");
+    var closeButton = document.getElementById("closeButton");
+    var triangle = document.getElementById("triangle");
+
+    if (container.classList.contains("hide")) {
+        container.classList.remove("hide");
+        closeButton.classList.remove("hide"); // 显示收起按钮
+        triangle.innerHTML = "▲"; // 改为倒三角
+    } else {
+        container.classList.add("hide");
+        closeButton.classList.add("hide"); // 隐藏收起按钮
+        triangle.innerHTML = "▼"; // 改回正三角
+    }
+}
+
+function closeImagesAndScrollToTriangle() {
+    var triangle = document.getElementById("triangle");
+    var container = document.getElementById("imageContainer");
+    var closeButton = document.getElementById("closeButton");
+
+    container.classList.add("hide");
+    closeButton.classList.add("hide");
+    triangle.innerHTML = "▼";
+    triangle.scrollIntoView({ behavior: 'smooth' });
+}
